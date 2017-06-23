c154c2d9009fe1e958bc5a1e52e93ab12f80ac58
Tamas K. Papp <tkpapp@gmail.com>
2017-06-23 09:26:53 +0200

    Various explorations.
    Still finding that posterior *variance* changes with the sample size
    M. More exploration is needed.

--- examples/toy_exponential_comparison.jl
+++ examples/toy_exponential_comparison.jl
@@ -5,15 +5,58 @@ using KernelDensity
 
 include(Pkg.dir("HamiltonianABC", "examples", "toy_exponential_problem.jl"))
 
-pp = ToyExponentialProblem(rand(Exponential(1), 100), 0, 10, 1000)
+λ₀ = 1.0
+y = rand(Exponential(λ₀), 1000)
 
-chain, a = mcmc(RWMH(fill(0.1,1,1)), pp, simulate_state(pp, [log(10)]), 10000)
+xs = linspace(λ₀*0.8,λ₀*1.2,100)
+plt = plot(xs, analytical_posterior(ToyExponentialProblem(y, 0, 10, 1)).(xs),
+           label = "true posterior")
+for m in 2:4
+    M = 10^m
+    p = ToyExponentialProblem(y, 0, 10, M)
+    chain, a = mcmc(RWMH(fill(0.1,1,1)), p, simulate_state(p, [log(λ₀)]), 10000)
+    λs = get_λ.(get_θ.(chain))
+    dens = kde(λs)
+    plot!(plt, xs, pdf.(dens, xs), label = "M = $(M)")
+end
+display(plt)
+
+vline!([mean(p.y)], label = "ML")
+vline!([mean(λs)], label = "posterior mean")
+(var(λs)/var(rand(analytical_distribution(p), 100000); corrected = false))^2
+
+
+
+######################################################################
+# comparison of MCMC and indirect posterior
+######################################################################
+
+M = 100
+p = ToyExponentialProblem(y, 0, 10, M)
+state = simulate_state(p, [log(λ₀)])
+chain, a = mcmc(RWMH(fill(0.1,1,1)), p, state, 10000)
 λs = get_λ.(get_θ.(chain))
 dens = kde(λs)
+f(x) = HamiltonianABC.logposterior(p, state, log(x))
+c = f(λ₀)-log(pdf(dens, λ₀))
+plt = plot(xs, x->exp(f(x)-c), label = "indirect posterior")
+plot(xs, pdf.(dens, xs), label = "MCMC, M = $(M)")
 
-plot(x->pdf(dens, x), xlim = (0,2), label = "posterior from RWMH")
-plot!(analytical_posterior(pp), label = "true posterior")
-vline!([mean(pp.y)], label = "ML")
-vline!([mean(λs)], label = "posterior mean")
+f(l)-c = log(pdf(dens, l))
 
-(var(λs)/var(rand(analytical_distribution(pp), 100000); corrected = false))^2
+exp(f(λ₀)-c)
+
+data_loglikelihood(p, mean_and_var(generate_x(p, [0.0], rand(100))))
+
+y = rand(Exponential(1),100)
+p = ToyExponentialProblem(y, 0, 10, 100)
+xs = linspace(0.5,2,100)
+plt = plot(xs, log.(analytical_posterior(p).(xs)), label = "true log posterior")
+for m in 2:4
+    M = 10^m
+    p = ToyExponentialProblem(y, 0, 10, M)
+    state = simulate_state(p, [log(1)])
+    plot!(plt, xs, x->HamiltonianABC.logposterior(p, state, log(x)),
+          label = "M=$(M)")
+end
+display(plt)
