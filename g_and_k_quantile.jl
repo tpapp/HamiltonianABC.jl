48502a66b0532517f3de200ff89bdf5eef1cc274
DoriszAlbrecht <dorisz.albrecht@gmail.com>
2017-07-09 18:55:21 +0200

    Quantile functions with tests.

--- g_and_k_quantile.jl
+++ g_and_k_quantile.jl
@@ -53,3 +53,32 @@ Return the logpdf of the g-and-k quantile function at x
 function get_pdf_quant(quant_cdf, x)
     log(derivative.(quant_cdf, x))
 end
+
+
+# the quantiles should coincide with the Normal(0,1)
+
+@testset "g-and-k quantile distribution" begin
+    # parameters of the g-and-k quantile function
+    χ = [0, 1, 0, 0, 1]
+    # cdf function that only depends on the xs, but not on the parameters
+    cdf_samples(x) = get_cdf_quant(normal_gk_quant(x,χ...), χ)
+    # generating standard normally distributed samples
+    xxs = rand(Normal(0, 1), 1000)
+    # comparing the quantiles
+    test_cdf(cdf_samples, xxs; ps = 0.1:0.1:0.9, atol = 0.05)
+end
+
+
+## testing for a t-distribution with 5 degrees of freedom
+
+@testset "g-and-k quantile for t-distribution" begin
+    # t-distribution with 5 degrees of freedom can be sampled
+    # with the following parameters in the g-and-k quantile function
+    χ = [0, 0.9, 0, 0.26]
+    # generating a t-distribution with 5 degrees of freedom
+    tx = rand(TDist(5), 1000)
+    # cdf
+    cdf_t_samples(x) = get_cdf_quant(normal_gk_quant(x,χ...), χ)
+    # comparing the quantiles
+    test_cdf(cdf_t_samples, tx; ps = 0.1:0.1:0.9, atol = 0.05)
+end
