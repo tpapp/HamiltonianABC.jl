cc2f2fd69a594be7b16f146d28aeb72f1c2dbeb2
Tamas K. Papp <tkpapp@gmail.com>
2017-07-23 20:20:46 +0200

    Minor changes to model.
    - moved package lines, removed ContinuousTransformations (for now)
    
    - rewrote simulation code:
    
      - more consistent docstring
    
      - get initial x from a ν scaled up by stationary volatility (this
        was important, otherwise the value was random!)
    
      - vectorized y code
    
    - renamed σ_v to σ to be more consistent
    
    - renamed OLS variance to v
    
    - removed superfluous eps() term from OLS
    
    - added *another* regression, using combined likelihood for
      fitting. this captures both first difference and level effects, and
      contributes to the excellent fit
    
    - added convenience simulation function
    
    - using 10000 samples: y itself is random from a stochastic volatility
      process, so the posterior means themselves change between
      simulations, using many samples diminishes this effect
    
    - plot cleanup
    
    - made test bounds narrower

--- test/stoch_vol_model_fixes.jl
+++ test/stoch_vol_model_fixes.jl
@@ -1,38 +1,37 @@
+using ArgCheck
 using Distributions
 using Parameters
 using HamiltonianABC
-using ContinuousTransformations
-using Plots
 using StatsBase
 using StatPlots                 # for kernel density
 import HamiltonianABC: logdensity, simulate!
 using Base.Test
+
+using Plots
 plotlyjs()                      # Tamas likes this, optional
 
 """
-    simulate_stochastic(ρ, σ_v, ϵs, ηs)
+    simulate_stochastic(ρ, σ_v, ϵs, νs)
 
-Take in the parameter values (ρ, σ_v) for the latent volatility process, the errors ϵs used for the measurement equation and the errors ηs used for the transition equation.
+Take in the parameter values (ρ, σ) for the latent volatility process, the errors ϵs used for the measurement equation and the errors νs used for the transition equation.
 
 The discrete-time version of the Ornstein-Ulenbeck Stochastic - volatility model:
 
-    y_t = x_t + ϵs_t where ϵs_t ∼ χ^2(1)
-    x_t = ρ * x_(t-1) + σ_v * ηs_t  where ηs_t ∼ N(0, 1)
+    y_t = x_t + ϵ_t where ϵ_t ∼ χ^2(1)
+    x_t = ρ * x_(t-1) + σ * ν_t  where ν_t ∼ N(0, 1)
 
 """
-function simulate_stochastic(ρ, σ_v, ϵs, ηs)
+function simulate_stochastic(ρ, σ, ϵs, νs)
     N = length(ϵs)
-    x_0 = rand(Normal(0, 1 / (1 - ρ^2)))
+    @argcheck N == length(νs)
     xs = Vector{Float64}(N)
-    ys = Vector{Float64}(N)
-    xs[1] = ρ * x_0 + σ_v * ηs[1]
-    ys[1] = xs[1] + log(ϵs[1]) + 1.27
-    for i in 2 : N
-        xs[i] = ρ * xs[i-1] + σ_v * ηs[i]
-        ys[i] = xs[i] + log(ϵs[i]) + 1.27
+    for i in 1:N
+        xs[i] = (i == 1) ? (νs[1]*σ*(1 - ρ^2)^(-0.5)) : (ρ*xs[i-1] + σ*νs[i])
     end
-    ys
+    xs + log.(ϵs) + 1.27
 end
+
+simulate_stochastic(ρ, σ, N) = simulate_stochastic(ρ, σ, rand(Chisq(1), N), randn(N))
 
 struct Toy_Vol_Problem
     "observed data"
@@ -40,22 +39,21 @@ struct Toy_Vol_Problem
     "prior for ρ (persistence)"
     prior_ρ
     "prior for σ_v (volatility of volatility)"
-    prior_σ_v
+    prior_σ
     "χ^2 draws for simulation"
     ϵ::Vector{Float64}
     "Normal(0,1) draws for simulation"
     ν::Vector{Float64}
 end
 
-
 """
-    Toy_Vol_Problem(ys, prior_ρ, prior_σ_v, M)
+    Toy_Vol_Problem(ys, prior_ρ, prior_σ, M)
 
 Convenience constructor for Toy_Vol_Problem.
 Take in the observed data, the priors, and number of simulations (M).
 """
-function Toy_Vol_Problem(ys, prior_ρ, prior_σ_v, M)
-    Toy_Vol_Problem(ys, prior_ρ, prior_σ_v, rand(Chisq(1), M), randn(M))
+function Toy_Vol_Problem(ys, prior_ρ, prior_σ, M)
+    Toy_Vol_Problem(ys, prior_ρ, prior_σ, rand(Chisq(1), M), randn(M))
 end
 
 """
@@ -70,15 +68,15 @@ function simulate!(pp::Toy_Vol_Problem)
 end
 
 """
-    OLS(y, x)
+    β, v = OLS(y, x)
 
-Take in the dependant variable (y) and the regressor (x), give back the estimated coefficients (β) and the variance (σ_2).
+Take in the dependant variable (y) and the regressor (x), give back the estimated coefficients (β) and the variance (v).
 """
 function OLS(y, x)
     β = x \ y
     err = (y - x * β)
-    σ_2 = mean(abs2, err) + eps()
-    return β, σ_2
+    v = mean(abs2, err)
+    return β, v
 end
 
 
@@ -96,77 +94,74 @@ lag_matrix(xs, ns, K = maximum(ns)) = hcat([lag(xs, n, K) for n in ns]...)
 @test lag_matrix(1:5, 1:3) == [3 2 1; 4 3 2]
 
 """
-    y, X = diff_yX(zs, K)
-
-Take the first differences of `zs`, then return conformable `y` and `X` matrices for OLS, where `X` is the first `K -1` lags of the first differences  plus a vector of ones and `y` starts at time `K+1`.
+    y, X = yX1(zs, K)
 """
-function diff_yX(zs, K)
+function yX1(zs, K)
     Δs = diff(zs)
-    lag(Δs, 0, K), hcat(lag_matrix(Δs, (K:-1:1), K), ones(length(Δs)-2))
+    lag(Δs, 0, K), hcat(lag_matrix(Δs, 1:K, K), ones(length(Δs)-K), lag(zs, 1, K+1))
+end
+
+function yX2(zs, K)
+    lag(zs, 0, K), hcat(ones(length(zs)-K), lag_matrix(zs, 1:K, K))
 end
 
 function logdensity(pp::Toy_Vol_Problem, θ)
-    @unpack ys, ϵ, prior_ρ, prior_σ_v, ν = pp
-    ρ, σ_v = θ
+    @unpack ys, prior_ρ, prior_σ, ν, ϵ = pp
+    ρ, σ = θ
     N = length(ϵ)
 
-    if (abs(ρ) > 1 - eps() || σ_v ≤ 0 )
+    if (abs(ρ) > 1 - eps() || σ ≤ 0 )
         return -Inf
     else
 
-        logprior = logpdf(prior_ρ, ρ) + logpdf(prior_σ_v, σ_v)
+        logprior = logpdf(prior_ρ, ρ) + logpdf(prior_σ, σ)
 
         # Generating xs, which is the latent volatility process
 
-        zs = simulate_stochastic(ρ, σ_v, ϵ, ν)
-        y, X = diff_yX(zs, 2)
+        zs = simulate_stochastic(ρ, σ, ϵ, ν)
+        β₁, v₁ = OLS(yX1(zs, 2)...)
+        β₂, v₂ = OLS(yX2(zs, 2)...)
 
         # We work with first differences
-        β, σ_2 = OLS(y, X)
-        yy, yX = diff_yX(ys, 2)
-        log_likelihood = sum(logpdf.(Normal(0, √(σ_2)), yy - yX * β))
-        logprior + log_likelihood
+        y₁, X₁ = yX1(ys, 2)
+        log_likelihood1 = sum(logpdf.(Normal(0, √v₁), y₁ - X₁ * β₁))
+        y₂, X₂ = yX2(ys, 2)
+        log_likelihood2 = sum(logpdf.(Normal(0, √v₂), y₂ - X₂ * β₂))
+        logprior + log_likelihood1 + log_likelihood2
     end
 end
 
 # Trial
-θ = [0.8, 1.2]
-y = simulate_stochastic(θ[1], θ[2], rand(Chisq(1), 1000), randn(1000))
-pp = Toy_Vol_Problem(y, Uniform(-1, 1), InverseGamma(1, 1), 1000)
+ρ = 0.8
+σ = 1.2
+y = simulate_stochastic(ρ, σ, 10000)
+pp = Toy_Vol_Problem(y, Uniform(-1, 1), InverseGamma(1, 1), 10000)
 
 # visualize posterior
-ρ_grid = linspace(-0.9, 0.9, 60)
-σ²_grid = linspace(0.1, 2, 50)
-logdensity_on_grid = ((ρ, σ²)->logdensity(pp, [ρ, σ²])).(ρ_grid, σ²_grid')
+ρ_grid = linspace(0.75, 0.9, 50)
+σ_grid = linspace(0.9, 1.4, 30)
+logdensity_on_grid = ((ρ, σ)->logdensity(pp, [ρ, σ])).(ρ_grid', σ_grid)
 logdensity_on_grid .-= maximum(logdensity_on_grid)
-heatmap(ρ_grid, σ²_grid, logdensity_on_grid', xlab = "ρ", ylab = "σ²",
-        title = "log posterior")
-
-heatmap(ρ_grid, σ²_grid, exp.(logdensity_on_grid'), xlab = "ρ", ylab = "σ²",
+heatmap(ρ_grid, σ_grid, exp.(logdensity_on_grid), xlab = "ρ", ylab = "σ",
         title = "posterior")
 
-
-logdensity(pp, θ)
-chain, a = mcmc(RWMH(diagm([0.02, 0.1])), pp, θ, 10000) # Σ was hand-tuned
-
-# Analysis with some plotting
-
+chain, a = mcmc(RWMH(diagm([0.02, 0.1])), pp, [ρ, σ], 10000) # Σ was hand-tuned
 posterior = vcat(chain[5000:end]'...)
-plot(posterior)
 
-cov(posterior, 1)    * 2/3           # used about 2/3 * this for Σ above
+# convergence diagnostics (visual)
+plot(posterior, label = ["ρ" "σ²"])
 
-plt = plot(density(posterior[:, 1]), label = "posterior", title = "rho")
-plot!(plt, a -> pdf(pp.prior_ρ, a), linspace(-1, 1, 100), label = "prior")
-vline!(plt, [θ[1]], label = "true value")
+# marginal posterior densities vs prior
+plt = plot(density(posterior[:, 1]), label = "posterior", title = "ρ")
+plot!(plt, a -> pdf(pp.prior_ρ, a), linspace(0.6, 1, 100), label = "prior")
+vline!(plt, [ρ], label = "true value")
 
-plt = plot(density(posterior[:, 2]), label = "posterior", title = "σ")
-plot!(plt, a -> pdf(pp.prior_σ_v, a), linspace(0, 5, 100), label = "prior")
-vline!(plt, [θ[2]], label = "true value")
-
-mean(posterior, 1)     # not bad, could be better
+plt = plot(density(posterior[:, 2]), label = "posterior", title = "σ²")
+plot!(plt, a -> pdf(pp.prior_σ, a), linspace(0, 5, 100), label = "prior")
+vline!(plt, [σ], label = "true value")
 
 @testset "first difference" begin
-    @test mean(posterior, 1)[1] ≈ θ[1] rtol = 0.30
-    @test mean(posterior, 1)[2] ≈ θ[2] rtol = 0.30
+    ρ̄, σ̄ = vec(mean(posterior, 1))
+    @test ρ̄ ≈ ρ rtol = 0.02
+    @test σ̄ ≈ σ rtol = 0.02
 end
