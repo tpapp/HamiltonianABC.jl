228677b740303f901610b07b445f77a332803ca8
Dorisz <dorisz.albrecht@gmail.com>
2017-08-08 17:52:46 +0200

    Type-stability

--- test/stoch_volatility.jl
+++ test/stoch_volatility.jl
@@ -11,6 +11,7 @@ using ReverseDiff
 using Plots
 using ContinuousTransformations
 using ProfileView
+import ForwardDiff: Dual
 
 plotlyjs()                      # Tamas likes this, optional
 
@@ -37,6 +38,11 @@ function simulate_stochastic(ρ, σ, ϵs, νs)
 end
 
 simulate_stochastic(ρ, σ, N) = simulate_stochastic(ρ, σ, rand(Chisq(1), N), randn(N))
+
+typeof(simulate_stochastic(1.0, 2.0, zeros(10), zeros(10))[1])
+@code_warntype simulate_stochastic(Dual(1.0,2.0), Dual(2.0,1.0),
+                                   zeros(10), zeros(10))
+
 
 struct Toy_Vol_Problem
     "observed data"
@@ -78,12 +84,18 @@ end
 Take in the dependant variable (y) and the regressor (x), give back the estimated coefficients (β) and the variance (v).
 """
 function OLS(y, x)
-    β::Vector{Float64} = x \ y
-    err::Vector{Float64} = (y - x * β)
-    v::Float64 = mean(abs2, err)
+    β = x \ y
+    err = y - x * β
+    v = mean(abs2, err)
     β, v
 end
-@code_warntype OLS(y, simulate_stochastic(ρ, σ, 10000))
+
+@code_warntype OLS(ones(3), ones(3,3))
+
+"an array of ForwardDiff.Dual, for testing type stability"
+duals(dims...) = fill(Dual(1.0, 2.0), dims...)
+
+@code_warntype OLS(duals(3), duals(3,3))
 
 ## In this form, we use an AR(2) process of the first differences with an intercept as the auxiliary model.
 
@@ -93,27 +105,49 @@ end
 Lag-`n` operator on vector `xs` (maximum `K` lags).
 """
 lag(xs, n, K) = xs[((K+1):end)-n]
+## seems type-stable
+@code_warntype lag(ones(5), 1,1)
 
-lag_matrix(xs, ns, K = maximum(ns)) = hcat([lag(xs, n, K) for n in ns]...)
+"""
+    lag_matrix(xs, ns, K = maximum(ns))
 
-@test lag_matrix(1:5, 1:3) == [3 2 1; 4 3 2]
+Matrix with differently lagged xs. 
+"""
+function lag_matrix(xs, ns, K = maximum(ns))
+    M = Matrix{eltype(xs)}(length(xs)-K, maximum(ns))
+    for i ∈ ns
+        M[:, i] = lag(xs, i, K)
+    end
+    M
+end
+
+## seems like that this lag_matrix function is type-stable now
+@code_warntype lag_matrix(ones(5), 1:3)
+@code_warntype lag_matrix(duals(3), 1:3)
+lag_matrix(1:5, 1:3) == [3 2 1; 4 3 2]
 
 
-## yX1 and yX2 makes Xs of type ANY
+
 "first auxiliary regression y, X, meant to capture first differences"
 function yX1(zs, K)
     Δs = diff(zs)
     lag(Δs, 0, K), hcat(lag_matrix(Δs, 1:K, K), ones(length(Δs)-K), lag(zs, 1, K+1))
 end
-@code_warntype yX1(simulate_stochastic(ρ, σ, 10000), 2)
+## now the yX1 function is type-stable
+@code_warntype yX1(simulate_stochastic(1.0, 2.0, zeros(10), zeros(10)), 2)
+@code_warntype yX1(simulate_stochastic(Dual(1.0,2.0),Dual(2.0,1.0), zeros(10), zeros(10)), 2)
 
 "second auxiliary regression y, X, meant to capture levels"
 function yX2(zs, K)
     lag(zs, 0, K), hcat(ones(length(zs)-K), lag_matrix(zs, 1:K, K))
 end
 
+## yX2 is type-stable as well now
+@code_warntype yX2(simulate_stochastic(1.0, 2.0, zeros(10), zeros(10)), 2)
+@code_warntype yX1(simulate_stochastic(Dual(1.0,2.0),Dual(2.0,1.0), zeros(10), zeros(10)), 2)
+
 function bridge_trans(dist::Distribution{Univariate,Continuous})
-    supp::Distributions.RealInterval = support(dist)
+    supp = support(dist)
     bridge(ℝ, minimum(supp) .. maximum(supp))
 end
 
@@ -125,12 +159,12 @@ function logdensity(pp::Toy_Vol_Problem, θ)
     @unpack ys, prior_ρ, prior_σ, ν, ϵ = pp
     trans = parameter_transformations(pp)
 
-    value_and_logjac::Float64 = [t(raw_θ, LOGJAC) for (t, raw_θ) in zip(trans, θ)]
+    value_and_logjac = [t(raw_θ, LOGJAC) for (t, raw_θ) in zip(trans, θ)]
 
-    par::Vector{Float64}= first.(value_and_logjac)
+    par= first.(value_and_logjac)
     ρ, σ = par
     N = length(ϵ)
-    logprior::Float64 = logpdf(prior_ρ, ρ) + logpdf(prior_σ, σ) + sum(last, value_and_logjac)
+    logprior = logpdf(prior_ρ, ρ) + logpdf(prior_σ, σ) + sum(last, value_and_logjac)
 
     # Generating xs, which is the latent volatility process
 
@@ -140,16 +174,15 @@ function logdensity(pp::Toy_Vol_Problem, θ)
 
     # We work with first differences
     y₁, X₁ = yX1(ys, 2)
-    log_likelihood1::Float64 = sum(logpdf.(Normal(0, √v₁), y₁ - X₁ * β₁))
+    log_likelihood1 = sum(logpdf.(Normal(0, √v₁), y₁ - X₁ * β₁))
     y₂, X₂ = yX2(ys, 2)
-    log_likelihood2::Float64 = sum(logpdf.(Normal(0, √v₂), y₂ - X₂ * β₂))
+    log_likelihood2 = sum(logpdf.(Normal(0, √v₂), y₂ - X₂ * β₂))
     logprior + log_likelihood1 + log_likelihood2
 
 end
 
 @code_warntype logdensity(pp, θ₀)
 ## problem is with :
-# yX1, yX2 -> X₁, X₂ is of type ANY
 # trans is ANY
 
 
@@ -168,7 +201,8 @@ logdensity(pp, θ₀)
 Profile.clear()
 @profile [logdensity(pp, θ₀) for i in 1:100];
 ProfileView.view()
-
+@profile [simulate_stochastic(ρ, σ, 10000) for i in 1:100];
+ProfileView.view()
 @code_warntype simulate_stochastic(ρ, σ, 10000)
 
 
@@ -181,7 +215,7 @@ loggradient(pp, θ₀)
 ## with reverse mode AD
 loggradient_rev(pp::Toy_Vol_Problem, x) = ReverseDiff.gradient(y->logdensity(pp, y), x)
 loggradient_rev(pp, θ₀)
-## seems like both work, but results are different
+## seems like both work AND results are close from the two methods!!!!!
 
 ## length, pp does not have an element such that i could get hold of the length of the parameter vector
 Base.length(::Toy_Vol_Problem) = 2.0
